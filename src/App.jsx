80457cfc98d3110dd5e42d07468045abee5732c4
SapientAsh <rowantreeofknowledge@gmail.com>
2023-02-13 13:37:58 -0800

    Progress so far

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import data from "./characters.json";
+import "./App.css";
+
+function Character({ name, hp, ac, initiative }) {
+  return (
+    <div className="character">
+      <p>Name: {name}</p>
+      <p>HP: {hp}</p>
+      <p>AC: {ac}</p>
+      <p>Initiative: {initiative}</p>
+    </div>
+  );
+}
+
+function CharacterWrapper({ characters }) {
+  const charArray = characters.map((character) => {
+    <Character
+      name={character["Name"]}
+      hp={character["HP"]}
+      ac={character["AC"]}
+      initiative={character["Initiative"]}
+    />
+  });
+
+  return (
+    <div className="character-wrapper">
+      {charArray}
+      <button onClick={() => console.log(charArray)}>Debug</button>
+    </div>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+      <h1>Characters</h1>
+      <CharacterWrapper characters={data["Characters"]} />
+    </>
+  );
+}
+
+/*
+characters.slice(index-2, index+2)
+*/
+
+export default App;
