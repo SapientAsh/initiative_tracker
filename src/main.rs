f86819eb8cb15ea130c9f09e68911af87d182d85
Rowan <rowantreeofknowledge@gmail.com>
2025-11-21 12:54:28 -0800

    Cleaned up formatting

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use serde::{Serialize, Deserialize};
-use std::{fmt,io::{self,Write}};
+use serde::{Deserialize, Serialize};
 use std::fs;
+use std::{
+    fmt,
+    io::{self, Write},
+};
 
 #[derive(Serialize, Deserialize)]
 struct JSONCharacter {
-    name : String,
-    ac   : u8,
-    hp   : u16
+    name: String,
+    ac: u8,
+    hp: u16,
 }
 
 struct Character {
-    name    : String,
-    ac      : u8,
-    current : u16,
-    max     : u16,
-    score   : u8,
-    temp    : u16
+    name: String,
+    ac: u8,
+    current: u16,
+    max: u16,
+    score: u8,
+    temp: u16,
 }
 
 impl Character {
     fn new(name: String, ac: u8, max: u16, score: u8) -> Self {
         Character {
-            name    : name,
-            ac      : ac,
-            current : max,
-            max     : max,
-            score   : score,
-            temp    : 0
+            name: name,
+            ac: ac,
+            current: max,
+            max: max,
+            score: score,
+            temp: 0,
         }
     }
 
@@ -50,59 +53,69 @@ impl Character {
 
     fn heal(&mut self, amount: u16) {
         self.current += amount;
-        if self.current > self.max { self.current = self.max }
+        if self.current > self.max {
+            self.current = self.max
+        }
     }
 }
 
 impl fmt::Display for Character {
-    fn fmt(&self, f: &mut fmt::Formatter<>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let hp_length = match self.temp > 0 {
-            true => self.current.to_string().len() + self.max.to_string().len() + self.temp.to_string().len() + 8,
-            false => self.current.to_string().len() + self.max.to_string().len() + 5
+            true => {
+                self.current.to_string().len()
+                    + self.max.to_string().len()
+                    + self.temp.to_string().len()
+                    + 8
+            }
+            false => self.current.to_string().len() + self.max.to_string().len() + 5,
         };
         let max_width = std::cmp::max(15, self.name.len() + 2);
         let max_width = std::cmp::max(max_width, hp_length + 1);
         let buffer = (max_width + self.name.len() % 2) % 2;
 
         let display_string = match self.temp > 0 {
-            true => format!("┌{}┐ \n\
+            true => format!(
+                "┌{}┐ \n\
             │{}{}{}│ \n\
             │ HP {}/{} + {}{}│ \n\
             │ AC {}{}│ \n\
             │ Init {}{}│ \n\
             └{}┘ \n\n",
-            "─".repeat(max_width),
-            " ".repeat((max_width - self.name.len()) / 2),
-            self.name,
-            " ".repeat((max_width - self.name.len()) / 2 + buffer),
-            self.current,
-            self.max,
-            self.temp,
-            " ".repeat(max_width - hp_length),
-            self.ac,
-            " ".repeat(max_width - 4 - self.ac.to_string().len()),
-            self.score,
-            " ".repeat(max_width - 6 - self.score.to_string().len()),
-            "─".repeat(max_width)
+                "─".repeat(max_width),
+                " ".repeat((max_width - self.name.len()) / 2),
+                self.name,
+                " ".repeat((max_width - self.name.len()) / 2 + buffer),
+                self.current,
+                self.max,
+                self.temp,
+                " ".repeat(max_width - hp_length),
+                self.ac,
+                " ".repeat(max_width - 4 - self.ac.to_string().len()),
+                self.score,
+                " ".repeat(max_width - 6 - self.score.to_string().len()),
+                "─".repeat(max_width)
             ),
-            false => format!("┌{}┐ \n\
+            false => format!(
+                "┌{}┐ \n\
             │{}{}{}│ \n\
             │ HP {}/{}{}│ \n\
             │ AC {}{}│ \n\
             │ Init {}{}│ \n\
             └{}┘ \n\n",
-            "─".repeat(max_width),
-            " ".repeat((max_width - self.name.len()) / 2),
-            self.name,
-            " ".repeat((max_width - self.name.len()) / 2 + buffer),
-            self.current,
-            self.max,
-            " ".repeat(max_width - hp_length),
-            self.ac,
-            " ".repeat(max_width - 4 - self.ac.to_string().len()),
-            self.score,
-            " ".repeat(max_width - 6 - self.score.to_string().len()),
-            "─".repeat(max_width))
+                "─".repeat(max_width),
+                " ".repeat((max_width - self.name.len()) / 2),
+                self.name,
+                " ".repeat((max_width - self.name.len()) / 2 + buffer),
+                self.current,
+                self.max,
+                " ".repeat(max_width - hp_length),
+                self.ac,
+                " ".repeat(max_width - 4 - self.ac.to_string().len()),
+                self.score,
+                " ".repeat(max_width - 6 - self.score.to_string().len()),
+                "─".repeat(max_width)
+            ),
         };
 
         write!(f, "{display_string}")
@@ -111,33 +124,33 @@ impl fmt::Display for Character {
 
 struct Node {
     data: Character,
-    next: Option<*mut Node>
+    next: Option<*mut Node>,
 }
 
 impl Node {
     fn new(data: Character) -> Self {
         Node {
-            data : data,
-            next : None
+            data: data,
+            next: None,
         }
     }
 }
 
 struct Initiative {
     head: Option<*mut Node>,
-    current: Option<*mut Node>
+    current: Option<*mut Node>,
 }
 
 impl Initiative {
     fn new() -> Self {
-        Initiative { 
-            head: None, 
-            current: None 
+        Initiative {
+            head: None,
+            current: None,
         }
     }
 
     fn add(&mut self, char: Character) {
-        unsafe{
+        unsafe {
             if self.head.is_none() {
                 self.head = Some(Box::into_raw(Box::new(Node::new(char))));
                 self.current = self.head;
@@ -151,14 +164,16 @@ impl Initiative {
                 return;
             }
 
-            if(*self.head.unwrap()).next.is_none() {
+            if (*self.head.unwrap()).next.is_none() {
                 let temp = Node::new(char);
                 (*self.head.unwrap()).next = Some(Box::into_raw(Box::new(temp)));
                 return;
             }
 
             let mut current = self.head;
-            while (*current.unwrap()).next.is_some() && (*(*current.unwrap()).next.unwrap()).data.score > char.score {
+            while (*current.unwrap()).next.is_some()
+                && (*(*current.unwrap()).next.unwrap()).data.score > char.score
+            {
                 current = (*current.unwrap()).next;
             }
 
@@ -238,7 +253,7 @@ impl Initiative {
             }
 
             let mut current = self.head;
-            while(*current.unwrap()).next.is_some() {
+            while (*current.unwrap()).next.is_some() {
                 if (*(*current.unwrap()).next.unwrap()).data.name == name {
                     (*current.unwrap()).next = (*(*current.unwrap()).next.unwrap()).next;
                     return;
@@ -276,34 +291,34 @@ impl Initiative {
     }
 
     fn import(&mut self, path: &str) -> Result<(), &'static str> {
-        let f: String =  match fs::read_to_string(path) {
+        let f: String = match fs::read_to_string(path) {
             Ok(v) => v,
-            Err(_) => return Err("Provided path is not valid")
+            Err(_) => return Err("Provided path is not valid"),
         };
 
-        let json_chars: Vec<JSONCharacter> = match serde_json::from_str(&f){
+        let json_chars: Vec<JSONCharacter> = match serde_json::from_str(&f) {
             Ok(v) => v,
-            Err(_) => return Err("The provided file is not JSON or is not in the expected format.")
+            Err(_) => {
+                return Err("The provided file is not JSON or is not in the expected format.");
+            }
         };
 
         println!("Enter initiative scores:");
-        json_chars.iter().for_each( | x | {
+        json_chars.iter().for_each(|x| {
             let mut score = prompt(format!("{}: ", x.name).as_str()).parse::<u8>();
 
             while score.is_err() {
                 score = prompt("Enter a number between 0-255: ").parse::<u8>();
             }
 
-            self.add(
-                Character {
-                    name    : x.name.clone(),
-                    ac      : x.ac,
-                    current : x.hp,
-                    max     : x.hp,
-                    score   : score.unwrap(),
-                    temp    : 0
-                }
-            );
+            self.add(Character {
+                name: x.name.clone(),
+                ac: x.ac,
+                current: x.hp,
+                max: x.hp,
+                score: score.unwrap(),
+                temp: 0,
+            });
         });
 
         Ok(())
@@ -320,10 +335,10 @@ impl Initiative {
         unsafe {
             while current.is_some() {
                 let char: &Character = &(*current.unwrap()).data;
-                let json_char = JSONCharacter{
-                    name : char.name.clone(),
-                    ac   : char.ac,
-                    hp   : char.max
+                let json_char = JSONCharacter {
+                    name: char.name.clone(),
+                    ac: char.ac,
+                    hp: char.max,
                 };
                 chars.push(json_char);
                 current = (*current.unwrap()).next;
@@ -333,7 +348,7 @@ impl Initiative {
         let json = serde_json::to_string(&chars).unwrap();
         let mut f: fs::File = match fs::File::create_new(path) {
             Ok(f) => f,
-            Err(_) => return Err("Path is invalid or file already exists")
+            Err(_) => return Err("Path is invalid or file already exists"),
         };
 
         if f.write_all(json.as_bytes()).is_err() {
@@ -351,7 +366,7 @@ impl Initiative {
 }
 
 impl fmt::Display for Initiative {
-    fn fmt(&self, f: &mut fmt::Formatter<>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         if self.head.is_none() {
             return write!(f, "Initiative order is empty \n");
         }
@@ -363,7 +378,7 @@ impl fmt::Display for Initiative {
                 current = (*current.unwrap()).next;
             }
         }
-        return Ok(())
+        return Ok(());
     }
 }
 
@@ -392,8 +407,9 @@ fn main() {
         println!();
 
         match input {
-            "help"    => {
-                println!("Available commands: \n\
+            "help" => {
+                println!(
+                    "Available commands: \n\
                 import: Add characters to initiative order from a compatible JSON file \n\
                 export: Save initiative order to JSON file that can be imported \n\
                 add: Add character to initiative order manually \n\
@@ -407,9 +423,10 @@ fn main() {
                 temp: Grant temporary HP to a specified character \n\
                 remove: Remove a specified character from the initiative order \n\
                 top: Set the current turn to the first in initiative order (useful after adding initial characters) \
-                ")
+                "
+                )
             }
-            "import"  => {
+            "import" => {
                 let path: String = prompt("Enter path to JSON: ");
 
                 let result: Result<(), &'static str> = init.import(path.as_str());
@@ -417,15 +434,15 @@ fn main() {
                     println!("{}", result.unwrap_err());
                 }
             }
-            "export"  => {
+            "export" => {
                 let path = prompt("Enter target path for JSON file: ");
 
-                let result: Result <(), &'static str> = init.export(path.as_str());
+                let result: Result<(), &'static str> = init.export(path.as_str());
                 if result.is_err() {
                     println!("{}", result.unwrap_err());
                 }
             }
-            "add"     => {
+            "add" => {
                 let name = prompt("Name: ");
                 let mut ac = prompt("AC: ").trim().parse::<u8>();
                 while ac.is_err() {
@@ -442,13 +459,18 @@ fn main() {
 
                 println!();
 
-                init.add(Character::new(name, ac.unwrap(), max.unwrap(), score.unwrap()));
+                init.add(Character::new(
+                    name,
+                    ac.unwrap(),
+                    max.unwrap(),
+                    score.unwrap(),
+                ));
             }
-            "next"    => {
+            "next" => {
                 init.advance();
                 init.display();
             }
-            "exit"    => {
+            "exit" => {
                 return;
             }
             "display" => {
@@ -457,12 +479,12 @@ fn main() {
             "current" => {
                 init.display();
             }
-            "show"    => {
+            "show" => {
                 let name = prompt("Name: ");
                 println!();
                 init.show(name);
             }
-            "damage"  => {
+            "damage" => {
                 let name = prompt("Name: ");
                 let mut amount = prompt("Amount: ").parse::<u16>();
                 while amount.is_err() {
@@ -470,7 +492,7 @@ fn main() {
                 }
                 init.damage(name, amount.unwrap());
             }
-            "heal"    => {
+            "heal" => {
                 let name = prompt("Name: ");
                 let mut amount = prompt("Amount: ").parse::<u16>();
                 while amount.is_err() {
@@ -478,7 +500,7 @@ fn main() {
                 }
                 init.heal(name, amount.unwrap());
             }
-            "temp"    => {
+            "temp" => {
                 let name = prompt("Name: ");
                 let mut amount = prompt("Amount: ").parse::<u16>();
                 while amount.is_err() {
@@ -486,19 +508,18 @@ fn main() {
                 }
                 init.temp(name, amount.unwrap());
             }
-            "remove"  => {
+            "remove" => {
                 let name = prompt("Name: ");
                 init.remove(name);
             }
-            "top"     => {
+            "top" => {
                 init.beginning();
                 init.display();
             }
-            _         => {
+            _ => {
                 println!("Sorry, I didn't understand that.");
             }
         };
         println!();
     }
 }
-
